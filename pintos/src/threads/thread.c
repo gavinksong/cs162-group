6180edb88c233c0224c7bf15bf58ffd62fe8011e
Jacobo Sternberg <j.sternberg@berkeley.edu>
2016-02-24 01:32:08 +0000

    modified initializers for the sleeper variables

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -463,7 +463,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
   t->magic = THREAD_MAGIC;
-
+  t->alarm_time = NULL;
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
