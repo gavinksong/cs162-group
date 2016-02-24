6595eb1a0a4026741c33c3aa67b674283dc445da
Jacobo Sternberg <j.sternberg@berkeley.edu>
2016-02-24 04:38:04 +0000

    added the base priority variable and initializer

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -462,6 +462,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  t->base_priority = priority;
   t->magic = THREAD_MAGIC;
 
   old_level = intr_disable ();
--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -89,11 +89,15 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    int priority;                       /* Effective Priority. */
+    int base_priority;                  /* Base Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+
+    /* The locks that are currently held by this thread. */
+    struct list held_locks;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
