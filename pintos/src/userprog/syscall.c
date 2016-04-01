60d11bbc17f3572466e903932c6b158a6bf95f8c
Jun Tan <jtan0325@berkeley.edu>
2016-04-01 01:59:53 +0000

    add getter functions for file system and implement filesize

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -30,24 +30,27 @@ syscall_handler (struct intr_frame *f UNUSED)
     thread_current ()->pnode->exit_status = args[1];
     thread_exit();
   }
-  else if (args[0] = SYS_PRACTICE) {
+  else if (args[0] == SYS_PRACTICE) {
   	f->eax = args[1] + 1;
   }
-  else if (args[0] = SYS_HALT) {
+  else if (args[0] == SYS_HALT) {
   	shutdown_power_off();
   }
-  else if (args[0] = SYS_CREATE) {
+  else if (args[0] == SYS_CREATE) {
   	lock_acquire(&file_lock);
     f->eax = filesys_create(args[1], args[2]);
     lock_release(&file_lock);
   }
-  else if (args[0] = SYS_REMOVE) {
+  else if (args[0] == SYS_REMOVE) {
   	lock_acquire(&file_lock);
     f->eax = filesys_remove(args[1]);
     lock_release(&file_lock);
   }
-  else if (args[0] = OPEN) {
+  else if (args[0] == OPEN) {
     f->eax = open_file(args[1]);
+  }
+  else if(args[0] == SYS_FILESIZE) {
+  	f->eax = get_filesize(args[1]);
   }
 }
 
@@ -55,11 +58,36 @@ int open_file(const char *file) {
 	lock_release(&file_lock);
   	struct file *file_instance_ = filesys_open(args[1]);
   	if (!file_instance_){
-       return -1;
+  		lock_release(&file_lock);
+        return -1;
   	}
   	int fd = add_file_to_process(file_instance_);
   	lock_release(&file_lock);
   	return fd;
+}
+
+int get_filesize(int fd) {
+	lock_acquire(&file_lock);
+	struct files *f = get_file_instance_from_fd(fd); 
+	if (!f) {
+		return -1;
+	} 
+	struct file *file_instance_ = f->file_instance;
+	int size = file_length(file_instance_);
+	lock_release(&file_lock);
+	return size;
+}
+
+struct files* get_file_instance_from_fd(int fd) {
+	struct thread* t = thread_current();
+	struct list_elem *e;
+	for(e = list_begin(&t->file_list); e != list_end(&t->file_list); e = list_next(e)){
+		struct files = list_entry(e, struct files, elem);
+		if (fd == files->fd){
+			return files;
+		}
+	}
+	return NULL;
 }
 
 int add_file_to_process(struct file *file_instance_) {
