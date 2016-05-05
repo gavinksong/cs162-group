65b72047c1f3e33b159ae8f905f83d45baea1346
Pengcheng <shawnpliu@berkeley.edu>
2016-05-05 01:36:56 +0000

    cleaner my-test-2

--- pintos/src/tests/filesys/base/my-test-2.c
+++ pintos/src/tests/filesys/base/my-test-2.c
@@ -9,25 +9,13 @@
 #define BLOCK_SIZE 512
 static char buf_a[BLOCK_SIZE];
 
-static void
-write_some_bytes (const char *file_name, int fd, const char *buf)
-{
-  size_t ret_val;
-  ret_val = write (fd, buf, BLOCK_SIZE);
-  if (ret_val != BLOCK_SIZE){
-    msg("WTF IT FAILS");
-    fail ("write %zu bytes in \"%s\" returned %zu",
-          BLOCK_SIZE, file_name, ret_val);
-  }
-}
-
 void
 test_main (void)
 {
   int fd;
   int n_read1;
   int n_read2;
-
+  size_t ret_val;
   random_init (0);
   random_bytes (buf_a, sizeof buf_a);
 
@@ -40,7 +28,10 @@ test_main (void)
   int i = 0;
   for(;i < 200; i++)
   {
-    write_some_bytes ("a", fd, buf_a);
+    ret_val = write (fd, buf_a, BLOCK_SIZE);
+    if (ret_val != BLOCK_SIZE)
+      fail ("write %zu bytes in \"a\" returned %zu",
+            BLOCK_SIZE, ret_val);
   }
   n_read2 = buffer_stat (2);
   msg("called block_read %d times in 200 writes", n_read2-n_read1);
